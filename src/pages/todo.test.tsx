7aadc8c81b7aace0312ea5a830e308e117308e32
Adam Birse <adambirse@gmail.com>
2021-10-03 17:18:37 +0100

    Add TODO page.

---
+++ src/pages/todo.test.tsx
@@ -0,0 +1,16 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+import TODO from "./todo";
+
+
+describe("TODO page", () => {
+  it("does render", () => {
+    shallow(<TODO />);
+  });
+  it("shows Heading", () => {
+    const wrap = shallow(<TODO />);
+    expect(wrap.find("h1").text()).toEqual("My TODOS");
+  });
+});
+export {};
---
+++ src/pages/todo.tsx
@@ -0,0 +1,5 @@
+export default function TODO() {
+  return (
+      <h1>My TODOS</h1>
+  );
+}
